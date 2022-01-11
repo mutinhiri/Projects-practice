7bd0256a9d7ec65498a3ca00ad314db895ff7d31
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-11 22:28:12 +0200

    Add icecream to the same state using one reducer

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const createStore = redux.createStore
 
 
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 function buyCake() {
   return  {
@@ -11,14 +12,23 @@ function buyCake() {
     }
 }
 
+function buyIceCream() {
+  return {
+    type : BUY_ICECREAM
+  }
+}
 const initialState = {
-  numOfCakes: 10
+  numOfCakes: 10,
+  numOfIceCreams: 20
 } 
 
 const reducer = (state= initialState, action) => {
   switch (action.type) {
     case BUY_CAKE: return {
       ...state, numOfCakes: state.numOfCakes -1
+    }
+    case BUY_ICECREAM: return {
+      ...state, numOfIceCreams: state.numOfIceCreams -1
     }
     default: return state
   }
@@ -31,5 +41,6 @@ const unsubscribe = store.subscribe(() => console.log('updates state', store.get
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
 unsubscribe()
 
