c1ec05dbeeb75e11ffb089015d82d0a19263ca10
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-11 23:13:26 +0200

    Add action creators

--- asyncApicall.js
+++ asyncApicall.js
@@ -1,6 +1,29 @@
+const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST'
+const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS'
+const FETCH_USERS_ERROR = 'FETCH_USERS_ERROR'
+
 const initialState = {
   oading: false,
   users: [],
   error: ''
 }
 
+const fetchUsersRequest = () => {
+  return {
+    type: FETCH_USERS_REQUEST
+  }
+}
+
+const fetchUsersSuccess = (users) => {
+  return {
+    type: FETCH_USERS_SUCCESS,
+    payload: users
+  }
+}
+
+const fetchUsersFailure = error => {
+  return {
+    type: FETCH_USERS_ERROR,
+    payload: error
+  }
+}
