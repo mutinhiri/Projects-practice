948f1fe1165b63fbd7b0064b15d3d0f0ce6f8221
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-12 13:12:11 +0200

    export reducer function

--- react-api/src/redux/cakes/cakeReducer.js
+++ react-api/src/redux/cakes/cakeReducer.js
@@ -4,11 +4,12 @@ const initialState = {
   numOfCakes : 10,
 }
 
-const cakeReducer = (state = initialState, action) => {
+ export const cakeReducer = (state = initialState, action) => {
   switch (action.type) {
     case BUY_CAKE: return {
       ...state, numOfCakes :state.numOfCakes -1 
     }
+    default: return state
   }
 
 }
