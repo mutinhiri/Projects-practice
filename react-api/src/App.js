8abce5895bf329319681280e182b31b259feacf6
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-12 13:21:20 +0200

    Add provider to app component

--- react-api/src/App.js
+++ react-api/src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import CakeContainer from './components/CakeContainer';
+import { Provider } from 'react-redux';
+import store from './redux/cakes/store';
 
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
+        <div className="App">
       <CakeContainer />
     </div>
+    </Provider>
+  
   );
 }
 
