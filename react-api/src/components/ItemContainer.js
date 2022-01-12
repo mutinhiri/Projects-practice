e4a40f67b0dd78747675fe4ae6b83870f73b4e2f
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-12 21:53:05 +0200

    Add dispatch to Props

--- react-api/src/components/ItemContainer.js
+++ react-api/src/components/ItemContainer.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { buyCake, buyIceCream } from '../redux'
 
 function ItemContainer(props) {
   return (
     <div>
-      <h2>item-{ props.item}</h2>
+      <h2>item-{props.item}</h2>
+      <button onClick={props.buyItem}>buy items</button>
     </div>
   )
 }
 
 const mapStateToProps = (state, ownProps) => {
-  const itemState = ownProps.cake ? state.cake.numOfCakes :
-    state.iceCream.numOfIceCream
+  const itemState = ownProps.cake ? state.cake.numOfCakes : state.iceCream.numOfIceCreams
+  
   return {
     item: itemState
   }
 }
 
-export default connect(mapStateToProps) (ItemContainer)
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const dispatchFunction = ownProps.cake ?()=> dispatch(buyCake()) : ()=>dispatch(buyIceCream())
+  return {
+    buyItem : dispatchFunction
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (ItemContainer)
