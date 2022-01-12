7b9cd8d88123475477844eaa5cb239af61ede8fd
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-12 20:35:47 +0200

    Add react redux dependency

--- react-api/src/App.js
+++ react-api/src/App.js
@@ -2,14 +2,13 @@ import './App.css';
 import CakeContainer from './components/CakeContainer';
 import { Provider } from 'react-redux';
 import store from './redux/cakes/store';
+import HooksCakeContainer from './components/HooksCakeContainer';
 
 function App() {
   return (
-    <Provider store={store}>
        <div className="App">
-        <CakeContainer />
+        <HooksCakeContainer/>
       </div>
-    </Provider>
   );
 }
 
